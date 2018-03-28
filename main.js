9b5b546372239990628d6660b05dab777d520cd4
Bohdan <bmushtuk@gmail.com>
2018-03-28 13:43:22 +0300

    email validation with jQuery

--- main.js
+++ main.js
@@ -9,4 +9,35 @@
             headerEl.classList.remove("scrolling");
         }
     });
-})(window);
+  })(window);
+// ------------------------- 
+
+// Email Validation
+// -------------------------
+function isEmail(email) {
+  var validEmail = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+  return validEmail.test(email);
+}
+jQuery(function($) {
+  var $form = $('form.main-form');
+  var $userInput = $form.find('input#input-email');
+  var $emailErrorMessage = $form.find('.wrong-email');
+  var $ctaBtn = $form.find('.read-more-btn');
+
+  // bind events
+  $form.submit(onSubmit);
+  
+  function onSubmit(event) {
+    event.preventDefault();
+    var emailValue = $userInput.val();
+    if( isEmail(emailValue) ) { // valid email
+      $emailErrorMessage.addClass('d-none');
+      $ctaBtn.removeClass('disabled');
+      console.log('email:', emailValue);
+    } else {
+      $emailErrorMessage.removeClass('d-none');
+      $ctaBtn.addClass('disabled');
+    }
+  }
+});
+// -------------------------
